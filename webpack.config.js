37026ef07a840eec5563459c787e42ebffce0e67
Aaron Min <min.aaron.min@gmail.com>
2017-02-14 21:01:29 -0800

    Modify folder hierarchy

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  entry: './client/index.js',
+  entry: './client/src/index.js',
   output: {
-    path: './public',
+    path: './client/dist',
     filename: 'bundle.js'
   },
   module: {
