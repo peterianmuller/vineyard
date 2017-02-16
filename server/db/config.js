7dec1fc635c0f6d02fe197c64cad36578de97d63
JARED SPENCER <jareddespencer@gmail.com>
2017-02-15 16:31:20 -0800

    Begin bookshelf refactor

--- server/db/config.js
+++ server/db/config.js
@@ -1,6 +1,40 @@
-import Sequelize from 'sequelize';
+import knex from 'knex';
+import bookshelf from 'bookshelf';
 
-export default new Sequelize('vineyard', null, null, {
-  dialect: 'postgres',
-  port: 5432
+var db = bookshelf(knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'vineyard'
+  }
+}));
+
+db.knex.schema.hasTable('organizations').then((exists) => {
+  if (!exists) {
+    db.knex.schema.createTable('organizations', (org) => {
+      org.increments('id').primary();
+      org.string('name', 255);
+      org.string('phone_umber', 20);
+      org.string('tier', 255)
+    }).then((table) => {
+      console.log('Created Table', table);
+    });
+  }
 });
+
+db.knex.schema.hasTable('vineyards').then((exists) => {
+  if (!exists) {
+    db.knex.schema.createTable('vineyards', (vineyard) => {
+      vineyard.increments('id').primary();
+      vineyard.string('name', 255).notNullable();
+      vineyard.string('phone_umber', 255);
+      vineyard.string('appellation', 255).notNullable();
+    }).then((table) => {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+export { db, knex };
--- server/db/models/organizations.js
+++ server/db/models/organizations.js
@@ -1,6 +1,20 @@
-import Sequelize from 'sequelize';
-import sequelize from '../config';
+import { db, knex } from '../config';
 import Addresses from './addresses';
+
+const Organization = bookshelf.Model.extend({
+  tableName: 'organizations',
+});
+
+db.knex.schema.hasTable('organizations').then((exists) => {
+  if (!exists) {
+    db.knex.schema.createTable('organizations', (org) => {
+      org.increments('id').primary();
+      org.string('name', 255);
+      org.string('phoneNumber', 20);
+      org.string('tier', 255)
+    });
+  }
+})
 
 const Organizations = sequelize.define('organization', {
   name: Sequelize.STRING,
