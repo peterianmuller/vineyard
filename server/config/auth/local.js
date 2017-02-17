e69fdda0f314d40682d29fd7c46f90cddbcba55b
Andrea Renee Oneto <andreareneeo@gmail.com>
2017-02-16 19:58:19 -0800

    set up passport object for authentication, added middleware
    next commit: will add in sessions

--- server/config/auth/local.js
+++ server/config/auth/local.js
@@ -1,1 +1,37 @@
 import passport from 'passport';
+import passportLocal from 'passport-local';
+
+import User from '../../db/models/users';
+
+const LocalStrategy = passportLocal.Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username })
+    .then(user => {
+    	if (!user) {
+    		return done(null, false, { message: 'Incorrect username.' });
+    	}
+    	if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    })
+    .catch(err => done(err))
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id)
+  .then(user => done(user))
+  .catch(err => done(err))
+});
+
+export default passport;
+
+
+
--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,3 +1,13 @@
-export default function middleware(app, express) {
+import session from 'express-sessions';
+import passport from './auth/local';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
+export default function middleware(app, express) {
+  app.use(cookieParser());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+  app.use(express.session({ secret: 'keyboard cat' }));
+	app.use(passport.initialize());
+  app.use(passport.session());
 }
