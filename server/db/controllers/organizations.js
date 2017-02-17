e7b75ace9bb7c396e29d00d2c6b79b7e8bb0e78e
JARED SPENCER <jareddespencer@gmail.com>
2017-02-16 16:33:07 -0800

    Built functions for organizations controller. Have not built updateOrganization.

--- server/db/controllers/organizations.js
+++ server/db/controllers/organizations.js
@@ -2,7 +2,9 @@ import Organizations from '../models/organizations';
 
 const newOrganization = (req, res, next) => {
   return Organizations.create({
-    // req.body
+    name: req.body.name,
+    phoneNumber: req.body.phoneNumber,
+    tier: req.body.tier
   })
   .then((organization) => {
     if (organization) {
@@ -15,10 +17,55 @@ const newOrganization = (req, res, next) => {
   });
 }
 
-const getOrganization = (req, res, next) => {};
-const getOrganizations = (req, res, next) => {};
-const updateOrganization = (req, res, next) => {};
-const deleteOrganization = (req, res, next) => {};
+const getOrganization = (req, res, next) => {
+  return Organizations.find({
+    where: {
+      name: req.body.name
+    }
+  })
+  .then((organization) => {
+    if (organization) {
+      // QUESTION: BETTER TO USE RES.SEND?
+      res.json(organization);
+    }
+    next();
+  }).catch((err) => {
+    console.log('could not find orginzation ', err);
+  });
+};
+
+const getOrganizations = (req, res, next) => {
+  return Organizations.findAll()
+  .then((organizations) => {
+    if (organizations) {
+      // QUESTION: BETTER TO USE RES.SEND?
+      res.json(organizations);
+    }
+    next();
+  }).catch((err) => {
+    console.log('could not find orginzations ', err);
+  });
+};
+// TODO: FIGURE OUT WHETHER TO USE UPDATE, OR TO FIND THE ORGANIZATION AND CHANGE THE DIFFERENT ATTRIBUTE.
+const updateOrganization = (req, res, next) => {
+  return Organizations.
+};
+
+const deleteOrganization = (req, res, next) => {
+  return Organizations.find({
+    where: {
+      name: req.body.name
+    }
+  })
+  .then((organization) => {
+    if (organization) {
+      organization.destroy();
+    }
+    next();
+  }).catch((err) => {
+    console.log('could not delete organization ', err);
+  });
+};
 
 export default {
   newOrganization,
