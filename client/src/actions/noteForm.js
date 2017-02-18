512863d1a4e54813d7b1d8f2dbc046108a3bdaa0
Peter Muller <peterianmuller@gmail.com>
2017-02-17 20:37:28 -0800

    Added persistence to db

--- client/src/actions/noteForm.js
+++ client/src/actions/noteForm.js
@@ -1,7 +1,29 @@
+import axios from 'axios';
+
 export function setNoteFormItem(item, value) {
   var toReturn = { value };
 
   toReturn.type = "SET_NOTE_FORM_" + item.toUpperCase();
 
   return toReturn;
+}
+
+//add in rest of cols in schema 
+export function postNote(note) {
+  return dispatch => axios.post('/api/note', {
+    title: note.title,
+    text: note.textArea,
+    location: '3',
+    image: ''
+  })
+  .then(() => dispatch(clearNoteFields()))
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
+function clearNoteFields() {
+  return {
+    type: "CLEAR_NOTE_FIELDS"
+  };
 }
