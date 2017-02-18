b6efe7b20be02696b426159fd7c223caa0d82e50
Peter Muller <peterianmuller@gmail.com>
2017-02-18 15:40:52 -0800

    Added basic skeleton for notes view

---
+++ client/src/containers/NotesView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Col, ControlLabel, FormControl, FormGroup, Grid, Row } from 'react-bootstrap';
+
+export default class NotesView extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Previous Forms Go Here</h1>
+      </div>
+    )
+  }
+}
