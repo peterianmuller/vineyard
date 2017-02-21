c5f72a80019f18ae136c10e10f0eed5647753dfb
Andrea Renee Oneto <andreareneeo@gmail.com>
2017-02-21 11:15:58 -0800

    postgres configure name/password reset again, fixed to null

--- server/db/config.js
+++ server/db/config.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('vineyard', 'postgres', '123', {
+const sequelize = new Sequelize('vineyard', null, null, {
   host: 'localhost',
   dialect: 'postgres',
   port: 5432
---
+++ test/client/tests/authTests.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ControlLabel, FormControl } from 'react-bootstrap';
+import { connect, Provider } from 'react-redux';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+// import session from 'supertest-session';
+// import myApp from '../../path/to/app';
+ 
