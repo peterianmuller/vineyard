ee534b77d365dae2f1d1b3176721f18a5b3dad29
Aaron Min <min.aaron.min@gmail.com>
2017-02-22 17:31:21 -0800

    Refactor auth functions into separate router

---
+++ server/config/auth/authFunctions.js
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken';
+import jwtOptions from '../auth/jwt';
+import { newUser } from '../../db/controllers/users';
+
+export function login(req, res) {
+  var payload = { id: req.user.id };
+  var token = jwt.sign(payload, jwtOptions.secretOrKey);
+  console.log('am i even in here', token);
+
+  res.status(201).json({ message: "OK", token: token });
+}
+
+export function logout(req, res) {
+  req.logout();
+  res.status(201).json(req.user);
+}
+
+export function sendUserIdFromJwt(req, res, next) {
+  res.status(200).json({ id: req.user.id });
+}
+
+export function register(req, res, next) {
+  const params = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    userName: req.body.userName,
+    password: req.body.password,
+    phoneNumber: req.body.phoneNumber,
+    email: req.body.email,
+    securityQuestion: req.body.securityQuestion,
+    securityAnswer: req.body.securityAnswer,
+    birthdate: req.body.birthdate,
+    accountRestrictions: req.body.accountRestrictions
+  };
+  return newUser(params)
+    .then((user) => {
+      console.log('huh', user);
+      next();
+    }).catch((err) => {
+      console.log('could not add user ', err);
+    });
+}
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,5 @@
 import path from 'path';
 import express from 'express';
-import jwt from 'jsonwebtoken';
-
-import jwtOptions from './auth/jwt';
-import { newUser } from '../db/controllers/users';
 
 // SERVER CONTROLLER DEPENDENCIES
 import organizationsRouter from './routes/organizations';
@@ -14,6 +10,7 @@ import rowsRouter from './routes/rows';
 import usersRouter from './routes/users';
 import notesRouter from './routes/notes';
 import alertsRouter from './routes/alerts';
+import authRouter from './routes/authorization';
 // import messagesController from './routes/messages';
 
 import { passport } from './auth/local';
@@ -21,14 +18,6 @@ import weatherRoutes from './weather/routes';
 
 export default function routes(app, express) {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
-
-  const checkAuthentication = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      next();
-    } else {
-      res.status(401).json('unauthorized access; please login');
-    }
-  };
 
   app.use('/api', passport.authenticate('jwt', { session: false }));
   app.use('/api/organization', organizationsRouter);
@@ -42,53 +31,7 @@ export default function routes(app, express) {
   app.use('/api/alert', alertsRouter);
   app.use('/api/weather', weatherRoutes);
 
-  var login = function(req, res) {
-    var payload = { id: req.user.id };
-    var token = jwt.sign(payload, jwtOptions.secretOrKey);
-    console.log('am i even in here', token);
-
-    res.status(201).json({ message: "OK", token: token });
-  };
-  // === LOGIN ROUTING ===
-  app.post('/auth/login',
-  passport.authenticate('local'), login);
-
-  // === LOGOUT ROUTING ===
-  app.get('/auth/logout', function(req, res){
-    req.logout();
-    res.status(201).json(req.user);
-  });
-
-  // === SESSION RETRIEVAL ===
-  app.get('/auth/session', 
-    passport.authenticate('jwt', { session: false }), 
-    (req, res, next) => {
-      res.status(200).json({ id: req.user.id });
-    }
-  );
-
-  app.post('/auth/register', (req, res, next) => {
-    const params = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      userName: req.body.userName,
-      password: req.body.password,
-      phoneNumber: req.body.phoneNumber,
-      email: req.body.email,
-      securityQuestion: req.body.securityQuestion,
-      securityAnswer: req.body.securityAnswer,
-      birthdate: req.body.birthdate,
-      accountRestrictions: req.body.accountRestrictions
-    };
-    return newUser(params)
-      .then((user) => {
-        console.log('huh', user);
-        next();
-      }).catch((err) => {
-        console.log('could not add user ', err);
-      });
-  }, passport.authenticate('local'), login);
-
+  app.use('/auth', authRouter);
 
   // === WILDCARD ROUTING ===
   app.use('*', (req, res, next) => {
