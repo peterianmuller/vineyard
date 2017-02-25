c65b4d6439740a47f8474be4d40d3b2e88aa1410
Andrea Renee Oneto <andreareneeo@gmail.com>
2017-02-25 14:44:50 -0800

    notes, org, addresses, and users populate all fields in db schemas

--- client/src/helpers/changeHandlers.js
+++ client/src/helpers/changeHandlers.js
@@ -22,6 +22,10 @@ export function setLatLong(lat, lon) {
   store.dispatch(setNoteFormItem('lon', lon)); 
 }
 
+export function setUserId (id) {
+  store.dispatch(setNodeFormItem('username', id));
+}
+
 
 export function authTransition(route, isTrue, nextState, replace, callback) {
 
