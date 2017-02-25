e468f93d22540eea4748f1c0c273f243b1aa3bcb
Peter Muller <peterianmuller@gmail.com>
2017-02-24 17:19:12 -0800

    Added test file for server routes

---
+++ test/client/tests/clientHttpTests.js
@@ -0,0 +1,16 @@
+import app from '../../../server/index.js';
+import supertest from 'supertest';
+import chai from 'chai';
+let expect = chai.expect;
+
+describe('GET /users', function() {
+  it('respond with json', function() {
+    return request(app)
+      .get('/users')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(response => {
+          assert(response.body.email, 'foo@bar.com')
+      })
+  });
+});
