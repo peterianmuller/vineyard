37a09c1253bba05fbd152725a1c75665d3d6fbf3
Aaron Min <min.aaron.min@gmail.com>
2017-02-24 17:35:42 -0800

    Modify view notes page format

--- client/src/containers/NotesView.js
+++ client/src/containers/NotesView.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 //UI
-import { Divider, Button, Grid } from 'semantic-ui-react';
+import { Button, Divider, Item } from 'semantic-ui-react';
 
 //Components
 import Note from '../components/Note';
@@ -11,35 +11,35 @@ import Note from '../components/Note';
 import { getNotes, addEvent } from '../actions/notesView';
 
 export default class NotesView extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  
   handleSubmit(e) {
     e.preventDefault();
-    this.props.dispatch(getNotes())
-    // this.props.dispatch(addEvent([{title:'title1', text:'Paleo gastropub mlkshk skateboard hell of synth. IPhone neutra tattooed helvetica sustainable, artisan humblebrag taxidermy tumblr. Vaporware pitchfork meh raclette DIY. Blue bottle chambray listicle subway tile, trust fund kale chips lumbersexual. Meh mlkshk yuccie, DIY brunch chia pop-up gluten-free literally austin kitsch cred keytar +1 ennui. Tbh pug gentrify glossier salvia yuccie coloring book. Jianbing aesthetic 8-bit portland.', lat: '122', lon: '22' }, {title:'title2', text: 'Marfa skateboard tilde, kogi chartreuse la croix polaroid edison bulb street art paleo narwhal deep v mustache cardigan. Polaroid quinoa post-ironic, sartorial stumptown forage heirloom hella plaid godard succulents. IPhone hot chicken sriracha, typewriter try-hard before they sold out umami blog lumbersexual authentic readymade disrupt small batch. Umami marfa gluten-free locavore, farm-to-table offal ethical mixtape enamel pin slow-carb bushwick etsy letterpress. Chicharrones unicorn occupy viral flexitarian, kitsch pop-up ramps bicycle rights twee fixie post-ironic direct trade hot chicken brunch. Chambray VHS vexillologist marfa photo booth chicharrones man bun butcher iPhone. Pabst jean shorts ramps, cronut +1 listicle squid blue bottle tofu lomo dreamcatcher fixie scenester.', lat: '89', lon: '100' }]));
+
+    this.props.dispatch(getNotes());
   }
 
   
   render() {
-    return(
+    return (
       <div>
-        <Button onClick={ this.handleSubmit.bind(this) }> Give me Notes!</Button>
+        <Button onClick={ this.handleSubmit.bind(this) }>Give me Notes!</Button>
         <Button>Filter</Button>
         <Divider />
-        <Grid columns="two" divided centered="true">
-          <Grid.Row>
-            <Grid.Column>
-            {this.props.notesView.map((note, key) => (
-              <Note title={note.title} text={note.text} lat={note.lat} lon={note.lon} key={key} />  
-            ))}
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
 
+        <Item.Group>
+          {
+            this.props.notesView.map((note, key) => (
+              <Note 
+                title={note.title} 
+                text={note.text} 
+                lat={note.lat} 
+                lon={note.lon} 
+                key={key} 
+              />  
+            ))
+          }
+        </Item.Group>
 
       </div>
-    )
+    );
   }
 }
