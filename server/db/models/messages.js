cb0463cf27039dc4b56bd0dc640ddb660db01e27
Andrea Renee Oneto <andreareneeo@gmail.com>
2017-02-28 12:16:35 -0800

    updated db schemas: added rooms model and schema accomodated messages and users to relate to rooms

--- server/db/models/messages.js
+++ server/db/models/messages.js
@@ -1,11 +1,15 @@
 import db from '../config';
 import Users from './users';
+import Rooms from './rooms';
 
 const Messages = db.Model.extend({
 	tableName: 'messages',
-	hasTimestamps: true,
+	hasTimestamps: false,
 	users: () => {
 		return this.belongsTo(Users);
+	},
+	room: () => {
+		return this.hasOne(Rooms);
 	}
 });
 
