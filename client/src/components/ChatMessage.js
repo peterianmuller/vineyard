13019fffda9656fcb755280bdc8c2cd8ad8bf987
Aaron Min <min.aaron.min@gmail.com>
2017-02-28 15:43:02 -0800

    Add dynamic rendering of borders on messages

--- client/src/components/ChatMessage.js
+++ client/src/components/ChatMessage.js
@@ -5,13 +5,23 @@ import React from 'react';
 import { Comment } from 'semantic-ui-react';
 
 export default props => {
+  var top = props.radius[1] === '5px';
+  var bottom = props.radius[0] === '5px';
+
+  var solo = props.radius[0] === '50px' && props.radius[1] === '50px';
+  var bottomOnly = bottom && !(bottom && top);
+  var topOnly = top && !(bottom && top);
+
   var aviStyle = {
     float: 'none'
   };
 
   var contentStyle = {
     width: '40%',
-    padding: '0.5em',
+    paddingRight: '0.5em',
+    paddingLeft: '0.5em',
+    marginRight: solo || bottomOnly ? '0px' : '35px',
+    marginTop: '0px',
   };
 
   var commentStyle = {
@@ -20,25 +30,48 @@ export default props => {
     flexAlign: props.left ? 'flex-start' : 'flex-end',
     textAlign: props.left ? 'left' : 'right',
     margin: 0,
-    padding: 0,
+    paddingTop: solo || topOnly ? '1.5em' : '0px',
+    paddingBottom: solo || bottomOnly ? '1.5em': '0px',
   };
 
   var commentTextStyle = {
     border: '0 solid black',
     backgroundColor: props.left ? '#42cbf4' : '#d6d6d6', 
-    borderRadius: props.left ? '5px 12% 12% 12%' : '12% 5px 12% 12%',
-    padding: '1.5em'
+    borderRadius: props.left ? 
+                  props.radius[0] + ' 50px 50px ' + props.radius[1] : 
+                  '50px ' + props.radius.join(' ') + ' 50px',
+    padding: '1.5em',
+    marginTop: '2px',
+    marginBottom: '2px'
   };
 
+  console.log(props.text, top, bottom);
   return (
     <Comment style={commentStyle}>
-    	<Comment.Avatar style={aviStyle}src='http://imgur.com/Ojc222a.jpg' />
+      {
+        solo || bottom && !(bottom && top) ? 
+          (<Comment.Avatar 
+             style={aviStyle}
+             src='http://imgur.com/Ojc222a.jpg' 
+           />) : ''
+      }
     	<Comment.Content style={contentStyle}>
-    	  <Comment.Author style={ { display: 'inline' } }>{ props.author }</Comment.Author> 
-    	  <Comment.Metadata style={ { display: 'inline' } }> Time info </Comment.Metadata>
+        { 
+          solo || top && !(bottom && top) ? 
+            (<Comment.Author style={ { display: 'inline' } }>
+               { props.author }
+             </Comment.Author>) : ''
+        }
+        { 
+          solo || top && !(bottom && top) ? (<Comment.Metadata style={ { display: 'inline' } }> 
+                  Time info 
+                </Comment.Metadata>) : ''
+        }
 
     	  <Comment.Text style={commentTextStyle}>
-    	    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt maximus eleifend. Mauris ornare arcu ultrices leo tincidunt euismod. Vivamus eget justo et leo ullamcorper euismod. Aliquam vitae dolor molestie, interdum quam id, fermentum sem. Donec pharetra tempor mi nec blandit. Phasellus ut aliquam risus. Morbi condimentum sapien nec ante laoreet, et condimentum augue feugiat. Ut auctor orci at ipsum semper consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec egestas nunc vel augue mollis, ac feugiat risus suscipit. In eget tellus elit. Nullam eu augue cursus, elementum leo sagittis, sollicitudin mauris. Aliquam at gravida lectus. Proin laoreet a urna sit amet commodo.
+          { 
+            props.text ? props.text : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt maximus eleifend. Mauris ornare arcu ultrices leo tincidunt euismod. Vivamus eget justo et leo ullamcorper euismod. Aliquam vitae dolor molestie, interdum quam id, fermentum sem. Donec pharetra tempor mi nec blandit. Phasellus ut aliquam risus. Morbi condimentum sapien nec ante laoreet, et condimentum augue feugiat. Ut auctor orci at ipsum semper consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec egestas nunc vel augue mollis, ac feugiat risus suscipit. In eget tellus elit. Nullam eu augue cursus, elementum leo sagittis, sollicitudin mauris. Aliquam at gravida lectus. Proin laoreet a urna sit amet commodo.'
+          }
     	  </Comment.Text>
     	</Comment.Content>
     </Comment>
--- client/src/components/MessageWindow.js
+++ client/src/components/MessageWindow.js
@@ -15,11 +15,63 @@ export default props => {
     width: '100%'
   };
     
+  var fakeMessages = [
+    {
+      author: 'Tim',
+      text: 'Adding another one above top one',
+      left: false
+    },
+    {
+      author: 'Tim',
+      text: 'This is the top one',
+      left: false
+    },
+    {
+      author: 'Tim',
+      text: 'This is the middle one',
+      left: false
+    },
+    {
+      author: 'Tim',
+      text: 'This is the second middle one',
+      left: false
+    },
+    {
+      author: 'Tim',
+      text: 'This is the bottom one',
+      left: false
+    }
+  ];
+
   return (
     <div style={ { display: 'flex', justifyContent: 'center' } }>
     	<Comment.Group style={rowStyle}>
-    	  <ChatMessage author='Aaron' left={true}/>
-    	  <ChatMessage author='Tim' left={false} />
+    	  <ChatMessage author='Aaron' left={true} radius={ ['50px',  '50px'] } />
+    	  <ChatMessage author='Tim' left={false} radius={ ['50px', '50px'] } />
+        {
+          fakeMessages.map((value, key) => {
+            var radius = ['50px', '50px'];
+
+            if (key > 0 && fakeMessages[key - 1].author === value.author) {
+              radius[0] = '5px';
+            } 
+            
+            if (key + 1 < fakeMessages.length && 
+                fakeMessages[key + 1].author === value.author) {
+              radius[1] = '5px'; 
+            }
+
+
+            return (
+              <ChatMessage 
+                key={key}
+                author={value.author}
+                text={value.text} 
+                left={!(value.author === 'Tim')} 
+                radius={radius}
+              />);
+          })
+        }
     	</Comment.Group>
     </div>
   );
