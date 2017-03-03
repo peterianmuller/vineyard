82fb01b3481f489356f2735aedcc0a71b54a1895
Andrea Renee Oneto <andreareneeo@gmail.com>
2017-03-02 16:54:27 -0800

    added draw plug in to map and made parser/iterator for data form insert to db

--- client/src/containers/UserMap.js
+++ client/src/containers/UserMap.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Circle, FeatureGroup } from 'react-leaflet';
+import { EditControl } from "react-leaflet-draw";
+
+let polyline;
+const subs = [ 'a', 'b', 'c', 'd' ];
+
 
 export default class MapView extends React.Component {
 	constructor(props) {
 		super(props)
     this.state = {
-      lat: 37.783,
-      lng: -122.409,
-      zoom: 10,
+      //vineyard coordinates
+      lat: 38.384,
+      lng: -122.865,
+      zoom: 20
     };
 	}
+
+  _onEditPath(e) {
+    console.log('Path edited !');
+  }
+
+  _onCreate(e) {
+    let type = e.layerType;
+    // polyline = e.layer;
+    let newPoly = e.layer._latlngs[0];
+    //polyline._latlngs[0] is the array of coordinates for that shape, 
+    //in the array, each index is a L.LatLng object that holds lat and lon
+    console.log('new user shape drawn: ', newPoly, 'layer type: ', type);
+    // To edit this polyline call : polyline.handler.enable()
+    console.log('Path created !');
+  }
+
+  _onDeleted(e) {
+    console.log('Path deleted !');
+  }
+
+  _mounted(drawControl) {
+    console.log('Component mounted !');
+  }
+
+  _onEditStart() {
+    console.log('Edit is starting !');
+  }
+
+  _onEditStop() {
+    console.log('Edit is stopping !');
+  }
+
+  _onDeleteStart() {
+    console.log('Delete is starting !');
+  }
+
+  _onDeleteStop() {
+    console.log('Delete is stopping !');
+  }
 
 	render() {
     const position = [this.state.lat, this.state.lng];
@@ -24,6 +69,29 @@ export default class MapView extends React.Component {
           <TileLayer
             url="https://api.mapbox.com/styles/v1/andipants12/cizsps6wg00842ro1wngxcqof/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5kaXBhbnRzMTIiLCJhIjoiY2l6b244ampwMDAxcDMzbnh5enpleTB2eCJ9.zu82GF0owfnb54lAGMUKKA"
             attribution='&copy;<a href="https://www.mapbox.com/about/maps" target="_blank">MapBox</a>, &copy;<a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
+          <FeatureGroup>
+            <EditControl
+              position='topright'
+              onEdited={this._onEditPath}
+              onCreated={this._onCreate}
+              onDeleted={this._onDeleted}
+              onMounted={this._mounted}
+              onEditStart={this._onEditStart}
+              onEditStop={this._onEditStop}
+              onDeleteStart={this._onDeleteStart}
+              onDeleteStop={this._onDeleteStop}
+              draw={{
+                rectangle: false
+              }}
+            />
+            <Circle center={[51.51, -0.06]} radius={200} />
+        </FeatureGroup>
+        <FeatureGroup>
+        </FeatureGroup>
+
+
+
+
         </Map>
       </div>
 		)
