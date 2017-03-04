81e7b428d648e48415030c94d514e20f908fad9d
Peter Muller <peterianmuller@gmail.com>
2017-03-04 11:59:23 -0800

    Clear data form after submission

--- client/src/actions/dataForm.js
+++ client/src/actions/dataForm.js
@@ -1,35 +1,35 @@
-// //React requirements
-// import { browserHistory } from 'react-router';
+//React requirements
+import { browserHistory } from 'react-router';
 
-// //AJAX
-// import axios from 'axios';
+//AJAX
+import axios from 'axios';
 
-// //other actions 
-// import { addToDataArray } from './dataArray';
-
-
-// export function appendDataFormItem(item, value) {
-//   var toReturn = { value };
-
-//   toReturn.type = "APPEND_DATA_FORM_" + item.toUpperCase();
-
-//   return toReturn;
-// }
-
-// export function postData(data) {
-//   // add to data array on client-side	
-//   console.log(data);	
-//   return addToDataArray(data);
-//   //return clearDataFormFields();
-// };
+//other actions 
+import { addToDataArray } from './dataArray';
 
 
+export function appendDataFormItem(item, value) {
+  var toReturn = { value };
 
-// function clearDataFormFields() {
-//   return {
-//     type: "CLEAR_DATA_FORM_FIELDS"
-//   };
-// }
+  toReturn.type = "APPEND_DATA_FORM_" + item.toUpperCase();
+
+  return toReturn;
+}
+
+export function postData(data) {
+  // add to data array on client-side	
+  console.log(data);	
+  return addToDataArray(data);
+  //return clearDataFormFields();
+};
+
+
+
+function clearDataFormFields() {
+  return {
+    type: "CLEAR_DATA_FORM_FIELDS"
+  };
+}
 
 
 
