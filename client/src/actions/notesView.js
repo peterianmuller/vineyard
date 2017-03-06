00d19036084ee0dfaa4f3e12b95458ae72d45cf1
Peter Muller <peterianmuller@gmail.com>
2017-03-06 11:46:27 -0800

    Able to sort notes by dateTime

--- client/src/actions/notesView.js
+++ client/src/actions/notesView.js
@@ -30,3 +30,43 @@ export function setNotes(data) {
     value: data
   };
 }
+
+export function getOldestDataArea(data) {
+  var plots = [];
+  var oldest = {time:'', lat:'', lng:'' };
+  // parse data and find oldest data by area
+    // calculate area of notes
+  console.log('data is', data);
+
+  data.forEach((note) => {
+    var dateTimeParsed = [], sortedNotes; 
+
+    dateTimeParsed.push(note.date_time.split('-')[0], note.date_time.split('-')[1], note.date_time.split('-')[2].split('T')[0]);
+   // dateTimeParsed = dateTimeParsed.join('');
+
+    plots.push({lat:note.latitude, lng:note.longitude, time: dateTimeParsed, title: note.title,text: note.text});
+
+  });
+
+
+  plots.push({lat:1, lng:1, time: ['2015','01', '03'], title: 'yas',text: 'yasss'});
+  plots.push({lat:1, lng:1, time: ['2017','01', '03'], title: 'yas',text: 'yasss'});
+
+
+  // sort notes by oldest
+  plots.sort((a,b) => {
+    if (a.time[0] < b.time[0] || a.time[1] < b.time[1] || a.time[2] < b.time[2]) {
+      return -1;
+    } else if (a.time[0] > b.time[0] || a.time[1] > b.time[1] || a.time[2] > b.time[2]) {
+      return 1;
+    } else {
+      return 0;
+    }   
+  });
+
+
+
+  console.log('plots is: ', plots);
+  
+
+}
