79ded3dd6a413f8981e8d39121857ef90416f54b
Andrea Renee Oneto <andreareneeo@gmail.com>
2017-03-08 10:24:19 -0800

    parsed data being added to the store properties next: dynamically render graph depending on data input adding user perferences buttons

--- client/src/reducers/dataVis.js
+++ client/src/reducers/dataVis.js
@@ -7,33 +7,33 @@ const defaultDataParams = {
 };
 
 
-export function dataVisReducer(state = [], action) {
+export function dataVisReducer(state = defaultDataParams, action) {
   switch(action.type) {
-    case "SET_ANALYSIS":
-      console.log(action.value);
-      return state.results.concat(action.value);
-    case "GET_ANALYSIS":
-      return action.value;
     case "SET_METHOD":
-    	return {
-    		...state,
-    		method: action.value
-    	}
+        return {
+            ...state,
+            method: action.value
+        }
     case "SET_VINEYARD":
-    	return {
-    		...state,
-    		vineyard: action.value
-    	}
+        return {
+            ...state,
+            vineyard: action.value
+        }
     case "SET_BLOCK":
-    	return {
-    		...state,
-    		block: action.value
-    	}
+        return {
+            ...state,
+            block: action.value
+        }
     case "SET_ROW":
-    	return {
-    		...state,
-    		row: action.value
-    	}
+        return {
+            ...state,
+            row: action.value
+        }
+    case "SET_ANALYSIS":
+        return {
+            ...state,
+            results: state.results.concat(action.value)
+        }
     case "CLEAR_FIELDS":
     	return {
     		...defaultDataParams
