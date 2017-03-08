d57b24e3bc5c9b8ccfef49ab8572460839190562
Peter Muller <peterianmuller@gmail.com>
2017-03-07 20:40:00 -0800

    Switched back to local host set up

--- client/src/actions/homeView.js
+++ client/src/actions/homeView.js
@@ -10,3 +10,32 @@ export function setFOrC() {
     type: "SET_F_OR_C",
   };
 }
+
+
+export function testOrgs() {
+    var context;
+    console.log('test orgs running')
+    axios.get('/api/organization/information', {
+      headers: {
+        'Authorization': 'JWT ' + localStorage.getItem('token')  
+      },
+      params: {
+        name: context.props.auth.username
+      }
+    })  
+    .then((res) => {
+      //here save vinyard name to local storage
+      console.log('this is the result from the axios call: ', res.data);
+      var vineyards = {
+        vineyards: res.data.vineyards
+      }
+      var org_info = {
+        orgs: res.data
+      }
+      window.localStorage.setItem('vineyards', JSON.stringify(vineyards));
+      window.localStorage.setItem('orgs', JSON.stringify(org_info));
+    })
+    .catch((err)=> {
+      console.log(err);
+    })
+  }
