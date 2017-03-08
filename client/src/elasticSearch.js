cb333af8db3fc7a464840a2c34082295af4d73fb
JARED SPENCER <jareddespencer@gmail.com>
2017-03-07 20:25:14 -0800

    Added environment variables for development and production stages.

--- client/src/elasticSearch.js
+++ client/src/elasticSearch.js
@@ -2,8 +2,11 @@ import elasticsearch from 'elasticsearch';
 
 var client = new elasticsearch.Client({
   host: {
-   host: 'search-vineyardes-ugmurjjdrnaiidevupbgaoudg4.us-west-1.es.amazonaws.com',
-   port: 80,
+  // FOR PRODUCTION:
+    // host: 'search-vineyardes-ugmurjjdrnaiidevupbgaoudg4.us-west-1.es.amazonaws.com',
+  // FOR DEVELOPMENT:
+    host: 'localhost',
+    port: 80,
     headers: {
       'Content-Type': 'text/plain'
     },
