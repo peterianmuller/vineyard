f48f45ad3c9ee9b0f452afe3c391300a7a538558
Andrea Renee Oneto <andreareneeo@gmail.com>
2017-03-09 14:05:28 -0800

    removed show oldest note code, since it's not being used

--- client/src/containers/NotesView.js
+++ client/src/containers/NotesView.js
@@ -15,25 +15,11 @@ export default class NotesView extends React.Component {
     e.preventDefault();
     this.props.dispatch(getNotes());
   }
-
-  presentOldestData(e) {
-    e.preventDefault();
-    // run algorithm to find oldest data and place it on screen
-    console.log('run algo');
-    // have an action to find oldest data
-    console.log('this.props is: ', this.props);
-
-    //update oldest notes by dispatching action to populate new store for oldest notes
-    
-    sortNotesByDate(this.props.notesView);
-  }
-
   
   render() {
     return (
       <div>
         <Button onClick={ this.handleSubmit.bind(this) }>Give me Notes!</Button>
-        <Button onClick={ this.presentOldestData.bind(this) }>Oldest Data</Button>
         <Divider />
 
         <Grid padded>
